8bcb4b2bfe81a71bb6c54beccce055818effef31
vengrammer <geronareven@gmail.com>
2025-08-30 10:05:32 +0800

    fix a problem in router

--- frontend/src/auth/RequireRole.jsx
+++ frontend/src/auth/RequireRole.jsx
@@ -1,7 +1,7 @@
 import { Outlet, Navigate } from "react-router-dom";
 
 function RequireRole() {
-  const roleAdmin = false;
+  const roleAdmin = true;
   return <>{roleAdmin ? <Outlet /> : <Navigate to="/unauthorized"/>  }</>;
 }
 export default RequireRole;
--- frontend/src/layout/LandingPageLayout.jsx
+++ frontend/src/layout/LandingPageLayout.jsx
@@ -1,21 +1,12 @@
 import { Button } from "@/components/ui/button";
 
 import { Login } from "@/components/custom/login";
+import { Outlet } from "react-router-dom";
 
 function LandingPageLayout() {
   return (
     <div className="w-full min-h-screen">
-      <div className="mx-7 md:w-150 md:h-150 my-10 md:mx-auto md:pt-auto align-items-center md:py-50 bg-zinc-200 p-10 ">
-        <span className="text-3xl">
-          Welcome to the{" "}
-          <span className="text-red-500">Tiniguiban Rural High School</span>{" "}
-          Online Grade Portal. Sign in to check your grades and stay connected
-          with your progress.
-        </span>
-        <div className="p-auto">
-          <Login />
-        </div>
-      </div>
+      <Outlet />
     </div>
   );
 }
